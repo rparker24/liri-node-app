cc2308ff34f6337bd7eb870a14c866b7b7216159
rparker24 <robert@robertmparker.net>
2016-04-13 01:20:30 -0400

    spotify working code, twitter not working

--- liri.js
+++ liri.js
@@ -1,15 +1,36 @@
 var keys = require('./keys.js');
 var fs = require('fs');
 var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
 
 var command = process.argv[2];
 
-// remove console logs when testing complete
+var tweetKeys = keys.twitterKeys;
+tweetKeys = new twitter;
+
 if(command == "my-tweets") {
-	console.log("tweets go here");
-	console.log(keys.twitterKeys.consumer_key);
+	var params = {screen_name: 'parkface13'};
+	debugger;
+	tweetKeys.get('statuses/user_timeline', params, function(error, tweets, response){
+	  if (!error) {
+	    console.log(tweets[0].text, tweets[0].created_at);
+	  }
+	});
 } else if(command == "spotify-this-song") {
 	var song = process.argv[3];
+	spotify.search({ type: 'track', query: song }, function(err, data) {
+	    if (err) {
+	        console.log('Error occurred: ' + err);
+	        return;
+	    } 
+	    var artist = data.tracks.items[0].artists[0].name;
+	    var songName = data.tracks.items[0].name;
+	    var album = data.tracks.items[0].album.name;
+	    var previewLink = data.tracks.items[0].preview_url;
+	    console.log(artist, songName, album, previewLink);
+	});
+
 	// not working - need to figure out how to handle no input for argv[3]
 	if(song == "") {
 		song = "What's My Age Again";
